6f048b9963580d8e9a35df66ea0adcae5705db1c
panforrest <guoqianp@gmail.com>
2017-03-24 13:36:23 -0400

    local api work with get method

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var helper = require('sendgrid').mail;
 
-/* GET users listing. */
+
+
 router.get('/:action', function(req, res, next) {
+
 	var action = req.params.action
 
 	if (action == 'send') {
-		res.json({
-			confirmation: 'success',
-			action: action
+		var from_email = new helper.Email('fpan@gmail.com');
+		var to_email = new helper.Email('pgq168@hotmail.com');
+		var subject = 'TEST';
+		var content = new helper.Content('text/html', 'Hello from fpan@gmail.com to pgq168@hotmail.com!');
+		var mail = new helper.Mail(from_email, subject, to_email, content);
+
+		var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+		var request = sg.emptyRequest({
+		    method: 'POST',
+		    path: '/v3/mail/send',
+		    body: mail.toJSON(),
+		});
+
+		sg.API(request, function(error, response) {
+	        if (error) {
+	        	res.json({
+	        		confirmation: 'fail',
+	        		message: error
+	        	})
+
+	            return
+	        }
+
+	        res.json({
+	        	confirmation: 'success',
+	        	response: response
+	        })	        
+			// return
 		})
 		return
 	}
@@ -16,7 +44,7 @@ router.get('/:action', function(req, res, next) {
 	res.json({
 		confirmation: 'fail',
 		message: 'invalid action'
-	})
+	})	
   
 });
 
